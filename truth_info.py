025e8c884a087a0621f2e918ebe7d6f0a3bf6d8a
ShyamB <bhuller.shyam@gmail.com>
2022-04-20 10:57:57 +0200

    added option to study beam MC

--- truth_info.py
+++ truth_info.py
@@ -17,11 +17,14 @@ import Plots
 def main():
     events = Master.Data(file) # load file
     if selection:
-        events.ApplyBeamFilter() # beam filter
-        valid = Master.Pi0MCMask(events, None) # two photon decay + > 1 shower
-        events.Filter([valid], [valid], False)
-        _, _, valid = events.MCMatching(applyFilters=False) # matching
-        events.Filter([valid], [valid], False)
+        if sampleType == "purePi0":
+            events.ApplyBeamFilter() # beam filter
+            valid = Master.Pi0MCMask(events, None) # two photon decay + > 1 shower
+            events.Filter([valid], [valid], False)
+            _, _, valid = events.MCMatching(applyFilters=False) # matching
+            events.Filter([valid], [valid], False)
+        if sampleType == "beam":
+            events = Master.BeamMCFilter(events)
 
     t = events.trueParticles.CalculatePhotonPairProperties() # calculate truth info
 
@@ -29,7 +32,11 @@ def main():
     if save is True:
         os.makedirs(outDir, exist_ok=True)
     for i in range(len(t_l)):
-        Plots.PlotHist(ak.ravel(t[i]), bins, xlabel=t_l[i], newFigure=True)
+        d = ak.ravel(t[i])
+        if len(t_r[i]) == 2:
+            d = d[d > t_r[i][0]]
+            d = d[d < t_r[i][1]]
+        Plots.PlotHist(d, bins, xlabel=t_l[i], newFigure=True)
         if save is True:
             Plots.Save(names[i], outDir)
     if save is False:
@@ -40,19 +47,23 @@ if __name__ == "__main__":
     names = ["inv_mass", "angle", "lead_energy", "sub_energy", "pi0_mom"]
     # plot labels
     t_l = ["True invariant mass (GeV)", "True opening angle (rad)", "True leading photon energy (GeV)", "True sub leading photon energy (GeV)", "True $\pi^{0}$ momentum (GeV)"]
+    t_r = [[]]*5
+    t_r = [[], [0, 1.5], [], [], []]
 
     parser = argparse.ArgumentParser(description="Plot truth information of pi0 decays")
     parser.add_argument(dest="file", type=str, help="ROOT file to open.")
     parser.add_argument("-b", "--nbins", dest="bins", type=int, default=50, help="number of bins when plotting histograms")
     parser.add_argument("-s", "--save", dest="save", action="store_true", help="whether to save the plots")
     parser.add_argument("-d", "--directory", dest="outDir", type=str, default="truth_info/", help="directory to save plots")
-    parser.add_argument("-e", "--event_selection", dest="selection", action="store_true", help="apply event selection (particle gun MC)")
-    args = parser.parse_args("work/ROOTFiles/pi0_multi_9_3_22.root -e".split()) #! to run in Jutpyter notebook
-    #args = parser.parse_args() #! run in command line
+    parser.add_argument("-e", "--event_selection", dest="selection", action="store_true", help="apply event selection")
+    parser.add_argument("-t", "--type", dest="sampleType", type=str, choices=["purePi0", "beam"], default="purePi0", help="Type of MC")
+    #args = parser.parse_args("work/ROOTFiles/Prod4a_6GeV_BeamSim_00.root -e -t beam".split()) #! to run in Jutpyter notebook
+    args = parser.parse_args() #! run in command line
 
     file = args.file
     bins = args.bins
     save = args.save
     outDir = args.outDir
     selection = args.selection
+    sampleType = args.sampleType
     main()
