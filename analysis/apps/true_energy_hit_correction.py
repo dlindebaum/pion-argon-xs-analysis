aa2fb282932c4fa7f8ab315215243fe44d84cead
ShyamB <bhuller.shyam@gmail.com>
2023-02-04 13:20:28 +0100

    added option to correct for all shared hits in the event

--- analysis/apps/true_energy_hit_correction.py
+++ analysis/apps/true_energy_hit_correction.py
@@ -33,8 +33,43 @@ def GetSharedEnergy(particleData : Master.ParticleData, e, a, b):
     sum_b = ak.sum(energy_b[energy_b > 0])
     return sum_a, sum_b
 
+
+def TrueParticleIndices(events, event, particle_number):
+    index = ak.local_index(events.trueParticlesBT.number[event])
+    return index[events.trueParticlesBT.number[event] == particle_number]
+
+
+def GetAllSharedEnergy(events : Master.Data, event : int, a : int):
+    mask = None
+    partner_energy = None
+    true_particles = events.trueParticlesBT.GetUniqueParticleNumbers(events.trueParticlesBT.number)
+
+    for i in range(ak.count(true_particles[event])):
+        index = TrueParticleIndices(events, event, true_particles[event][i])[0]
+
+        if events.trueParticlesBT.number[event][a] == events.trueParticlesBT.number[event][index]: continue
+
+        m = GetSharedHitsMask(events.trueParticlesBT, event, index, a)
+
+        if mask is None:
+            mask = m
+            partner_energy = ak.unflatten(ak.where(m, events.trueParticlesBT.energy[event][index], 0), 1, -1)
+        else:
+            mask = mask | m
+            partner_energy = ak.concatenate([partner_energy, ak.unflatten(ak.where(m, events.trueParticlesBT.energy[event][index], 0), 1, -1)], -1)
+    print(f"number of shared hits: {ak.count(mask[mask])}")
+
+    true_energy = events.trueParticlesBT.energy[event][a]
+    weights = true_energy / (true_energy + ak.sum(partner_energy, -1))
+
+    hit_energy = events.trueParticlesBT.hit_energy[event][a]
+    mask = mask & (hit_energy > 0)    
+    
+    excess_energy = ak.sum(hit_energy[mask] * (1 - weights[mask]))
+    return excess_energy
+
 @Master.timer
-def GetEnergy(events : Master.Data, showers : ak.Array):
+def GetEnergy(events : Master.Data, showers : ak.Array, all_energy : bool = False):
     data = {
         "true_energy_0" : [],
         "true_energy_1" : [],
@@ -46,7 +81,11 @@ def GetEnergy(events : Master.Data, showers : ak.Array):
         "shared_energy_1" : [],
     }
     for i in range(ak.num(showers, 0)):
-        e1, e2 = GetSharedEnergy(events.trueParticlesBT, i, showers[i][0], showers[i][1])
+        if all_energy:
+            e_0 = GetAllSharedEnergy(events, i, showers[i][0])
+            e_1 = GetAllSharedEnergy(events, i, showers[i][0])
+        else:
+            e_0, e_1 = GetSharedEnergy(events.trueParticlesBT, i, showers[i][0], showers[i][1])
         # if e1 <= 0 or e2 <= 0: continue
         true_energy = [events.trueParticlesBT.energy[i][showers[i][j]] for j in range(2)]
         true_energy_hits = [events.trueParticlesBT.energyByHits[i][showers[i][j]] for j in range(2)]
@@ -55,8 +94,8 @@ def GetEnergy(events : Master.Data, showers : ak.Array):
         data["true_energy_1"].append(true_energy[1])
         data["true_energy_hits_0"].append(true_energy_hits[0])
         data["true_energy_hits_1"].append(true_energy_hits[1])
-        data["shared_energy_0"].append(e1)
-        data["shared_energy_1"].append(e2)
+        data["shared_energy_0"].append(e_0)
+        data["shared_energy_1"].append(e_1)
     return data 
 
 
@@ -69,15 +108,16 @@ def main(args):
     start_shower_indices = ak.local_index(events.recoParticles.number)[mask]
     print(start_shower_indices)
 
-    df = pd.DataFrame(GetEnergy(events, start_shower_indices))
+    df = pd.DataFrame(GetEnergy(events, start_shower_indices, args.all_energy))
     df.to_csv(args.csv, mode = "a", header = not os.path.exists(args.csv))
     return
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = "run the hit correction study", formatter_class = argparse.RawDescriptionHelpFormatter)
-    parser.add_argument(dest="file", type=str, help="ROOT file to open.")
-    parser.add_argument("-e", "--events", dest="nEvents", type=int, nargs=2, default=[-1, 0], help="number of events to analyse and number to skip (-1 is all)")
-    parser.add_argument("-o", "--out-csv", dest="csv", type=str, default=None, help="output csv filename (will default to whatever type of data is produced)")
+    parser.add_argument(dest = "file", type = str, help = "ROOT file to open.")
+    parser.add_argument("-e", "--events", dest = "nEvents", type = int, nargs = 2, default = [-1, 0], help = "number of events to analyse and number to skip (-1 is all)")
+    parser.add_argument("-o", "--out-csv", dest = "csv", type = str, default = None, help = "output csv filename")
+    parser.add_argument("-a", "--all-energy", dest = "all_energy", action = "store_true", help = "get all shared energy or just shared energy between shower pairs")
     args = parser.parse_args()
     print(vars(args))
 
