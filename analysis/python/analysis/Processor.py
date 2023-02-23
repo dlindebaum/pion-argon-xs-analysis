3431b3e75fedaf8edf0d86437b8aeade6bd9a345
ShyamB <bhuller.shyam@gmail.com>
2023-02-23 18:58:21 +0100

    add log file and debug mode as default options

--- analysis/python/analysis/Processor.py
+++ analysis/python/analysis/Processor.py
@@ -33,8 +33,10 @@ class Processor(ABC):
         """
         self.parser = argparse.ArgumentParser(description = description, formatter_class = argparse.RawDescriptionHelpFormatter, epilog = None)
         self.parser.add_argument(dest = "file", type = str, help = "ROOT file to open.") # every process should take an input file
-        self.parser.add_argument(dest = "nBatches", type = int, help = "number of batches to run in") # every process must have a number of batches to do the task in
-        self.parser.add_argument("-c", "--cpu", dest = "cpu", type = int, default = 1, help = f"number of threads to use (max is {os.cpu_count()})") # this is optional but Processors will natively support multiprocessing so always leave this in as an option.
+        self.parser.add_argument(dest = "nBatches", type = int, help = "Number of batches to run in") # every process must have a number of batches to do the task in
+        self.parser.add_argument("-c", "--cpu", dest = "cpu", type = int, default = 1, help = f"Number of threads to use (max is {os.cpu_count()})") # this is optional but Processors will natively support multiprocessing so always leave this in as an option.
+        self.parser.add_argument("-l", "--log", dest = "log", action = "store_true", help = "Create log file for each process to be made")
+        self.parser.add_argument("-d", "--debug", dest = "debug", action = "store_true", help = "Debug Processor, will not actually run the process.")
 
         #* an example command line arguement the user could add.
         # self.parser.add_argument("-o", "--out-csv", dest = "csv", type = str, default = "out", help = "output csv filename")
@@ -45,13 +47,16 @@ class Processor(ABC):
         """ Parses the CLI, allowing for the user to configure how the arguement is parsed.
         """
         self.args = self.parser.parse_args()
+
         #* example of what parsing should be done here
+        if self.args.log:
+            os.makedirs("log/", exist_ok=True)
         # self.args.csv += ".csv"
         # print(vars(self.args))
         pass
 
     @abstractmethod
-    def Process(self, file : str, nEvents : int, start : int) -> subprocess.Popen:
+    def Process(self, file : str, nEvents : int, start : int, pid : int) -> subprocess.Popen:
         """ Process is the subprocess which will run a particular script or method, and must return a
             subrproccess.Popen in order for Run() to work (see below).
             The supplied arguements are must haves for any Processor. For an example see DummyProcess()
@@ -67,13 +72,14 @@ class Processor(ABC):
         pass
 
     @staticmethod
-    def DummyProcess(self, file : str, nEvents : int, start : int) -> subprocess.Popen:
+    def DummyProcess(self, file : str, nEvents : int, start : int, pid : int) -> subprocess.Popen:
         """ Example Process for testing purposes.
 
         Returns:
             subprocess.Popen: subprocees to interface with
         """
-        return subprocess.Popen(["sleep", "1"])
+        output = subprocess.DEVNULL if not self.args.log else open("log/" + f"dummy-process-{pid}.log", "w")
+        return subprocess.Popen(["sleep", "1"], stdout = output, stderr = subprocess.STDOUT)
     
 
     def CalculateBatches(self):
@@ -89,11 +95,14 @@ class Processor(ABC):
             self.batch_size = 1
         else:
             # set batch size to be equal, and the remainder is put into a new batch
-            #? split the remainder evenly across batches?
             self.batch_size = self.nEvents // self.args.nBatches
             self.batches = [self.batch_size] * self.args.nBatches
-            r = self.nEvents - (self.batch_size * self.args.nBatches)
-            if r > 0: self.batches.append(r)
+            remainder = self.nEvents % self.args.nBatches
+            
+            while remainder > 0:
+                self.batches[remainder % self.args.nBatches] += 1
+                remainder -= 1
+
         print(f"{self.batches=}")
 
     @timer
@@ -105,20 +114,22 @@ class Processor(ABC):
         chunks = ceil(len(self.batches) / self.args.cpu) # chunks is the number of batches to run per CPU
         print(f"{chunks=}")
 
-        counter = 0
-        start = 0
-        for i in range(chunks):            
-            # start each process per cpu simultaneously
-            processes = [] 
-            for c in range(self.args.cpu):
-                if start >= len(self.batches): break # if we don't need to use all our CPUs, stop
-                print(f"started process {start}")
-                processes.append(self.Process(self.args.file, self.batches[start], self.batch_size * start))
-                start += 1
-            # check the status of each job iteratively
-            for p in processes:
-                p.communicate()
-                print(f"finished process: {p}")
-                counter += 1
-            print(f"chunk {i + 1} done")
-        print(f"done {counter} batches")
+        if self.args.debug == False:
+            print(f"{self.args.debug=}")
+            counter = 0
+            start = 0
+            for i in range(chunks):            
+                # start each process per cpu simultaneously
+                processes = [] 
+                for c in range(self.args.cpu):
+                    if start >= len(self.batches): break # if we don't need to use all our CPUs, stop
+                    print(f"started process {start}")
+                    processes.append(self.Process(self.args.file, self.batches[start], self.batch_size * start, start))
+                    start += 1
+                # check the status of each job iteratively
+                for p in processes:
+                    p.communicate()
+                    print(f"finished process: {p}")
+                    counter += 1
+                print(f"chunk {i + 1} done")
+            print(f"done {counter} batches")
