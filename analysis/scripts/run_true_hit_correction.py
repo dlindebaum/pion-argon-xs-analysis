43896024df6b748656f88e8905957d9e8ac4ac6c
ShyamB <bhuller.shyam@gmail.com>
2023-02-23 19:02:52 +0100

    update

--- analysis/scripts/run_true_hit_correction.py
+++ analysis/scripts/run_true_hit_correction.py
@@ -13,21 +13,23 @@ class RunAnalysis(Processor):
     def Create_CLI(self, description: str = "Process description", eplilog: str = None):
         super().Create_CLI(description, eplilog)
         self.parser.add_argument("-o", "--out-csv", dest = "csv", type = str, default = "out", help = "output csv filename")
+        self.parser.add_argument("-a", "--all_energy", dest = "all_energy", action = "store_true", help = "get all shared energy or just shared energy between shower pairs")
         return
         
     def ParseCLI(self):
         super().ParseCLI()
-        self.args.csv += ".csv"
+        if ".csv" not in self.args.csv:
+            self.args.csv += ".csv"
         print(vars(self.args))
         return
 
-    def Process(self, file: str, nEvents: int, start: int) -> subprocess.Popen:
-        return subprocess.Popen([
-            "./true_energy_hit_correction.py",
-            file,
-            "-o", f"{self.args.csv}", 
-            "-e", f"{nEvents}", f"{start}"], 
-            stdout = subprocess.DEVNULL, stderr = subprocess.STDOUT)
+    def Process(self, file: str, nEvents: int, start: int, pid : int) -> subprocess.Popen:
+        output = subprocess.DEVNULL if not self.args.log else open("log/" + f"merge-quantities-{pid}-{nEvents}-{start}.log", "w")
+        cmd = ["true_energy_hit_correction.py", file, "-o", f"{self.args.csv}", "-e", f"{nEvents}", f"{start}"]
+        if self.args.all_energy:
+            cmd += ["-a"]
+        print(cmd)
+        return subprocess.Popen(cmd, stdout = output, stderr = subprocess.STDOUT)
 
 # having main() feels a little extra, but why not?
 def main(analysis : Processor):
