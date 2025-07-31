1a48c6a8d40d02308c082670275c75464c98f327
ShyamB97 <bhuller.shyam@gmail.com>
2025-07-31 16:49:34 +0100

    move notebook utils to separate module

---
+++ analysis/python/analysis/NotebookUtils.py
@@ -0,0 +1,25 @@
+"""
+Created on: 21/01/2024 17:14
+
+Author: Shyam Bhuller
+
+Description: random functions which are sometimes useful in notebooks
+"""
+
+from IPython.core.magic import register_line_magic
+from IPython import get_ipython
+
+@register_line_magic
+def init_notebook(line):
+    ipython = get_ipython()
+    ipython.run_line_magic("matplotlib", "inline") # %matplotlib inline
+    ipython.run_line_magic("load_ext", "autoreload") # %load_ext autoreload
+    ipython.run_line_magic("autoreload", "2") # %autoreload 2
+
+    #* magic to add python path to notebook environment
+    import sys
+    for pypath in sys.path:
+        if pypath.split("/")[-1] == "analysis": break
+    print(pypath)
+    ipython.run_line_magic("env", "PYTHONPATH $pypath") # %env PYTHONPATH $pypath
+    return line
