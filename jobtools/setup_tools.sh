f28cbe3b1463a286c11f8b2c2ab33517d48945a3
ShyamB97 <bhuller.shyam@gmail.com>
2026-02-13 12:47:26 +0000

    new file structure

--- jobtools/setup_tools.sh
+++
@@ -1,2 +0,0 @@
-MY_DIR=`realpath $BASH_SOURCE | xargs dirname`
-export PATH=$MY_DIR/tools:$PATH
--- jobtools/tools/split_filelist.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#* split text file by number of lines and then rename them add txt extension.
-
-split $1 -l $2
-
-for i in $(ls xa*)
-do
-    mv $i $i.txt
-done
